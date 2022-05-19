46a5a9510649056bec600f48ebf667bfadeeb47d
Aubron Wood <aubron@daemon.group>
2022-05-19 13:27:56 +0000

    Test deploying with new lambda URL system

--- graff/graphql.ts
+++ graff/graphql.ts
@@ -1,36 +1,31 @@
 // graphql.js
 
-const { ApolloServer, gql } = require('apollo-server-lambda');
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient();
-// Construct a schema, using GraphQL schema language
-// TODO: Figure out how to use prisma types here where useful.
-const typeDefs = gql`
-  type Query {
-    hello: String
-    users: [User]!
-  }
+import { ApolloServer, gql } from 'apollo-server-lambda';
+import { PrismaClient } from '@prisma/client';
+import { loadSchema } from '@graphql-tools/load';
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
 
-  type User {
-    id: String!
-    email: String
-    role: ROLE!
-  }
 
-  enum ROLE {
-    USER
-    ADMIN
-  }
-`;
+const main = async () => {
+  
+  const prisma = new PrismaClient();
+  // Construct a schema, using GraphQL schema language
+  // TODO: Figure out how to use prisma types here where useful.
+  const typeDefs = await loadSchema('graff.graphql',{
+    loaders: [new GraphQLFileLoader()]
+  })
+  
+  // Provide resolver functions for your schema fields
+  const resolvers = {
+    Query: {
+      hello: () => 'Hello world!',
+      users: () => prisma.user.findMany()
+    },
+  };
+  
+  
+  const server = new ApolloServer({ typeDefs, resolvers, csrfPrevention: true });
+  return server.createHandler();
+}
 
-// Provide resolver functions for your schema fields
-const resolvers = {
-  Query: {
-    hello: () => 'Hello world!',
-    users: () => prisma.user.findMany()
-  },
-};
-
-const server = new ApolloServer({ typeDefs, resolvers, csrfPrevention: true });
-
-exports.graphqlHandler = server.createHandler();
+exports.graphqlHandler = main();
